44003fb327923404bb57825870d98246f05ecb05
Vinay Hegde <76780478+vinayhegde105@users.noreply.github.com>
2024-02-01 13:31:30 +0530

    Update tmpl.go

--- tmpl.go
+++ tmpl.go
@@ -103,17 +103,9 @@ func executeTemplate(t *template.Template, d licenseData, top, mid, bot string) 
 
 const tmplApache = `Copyright{{ if .Year }} {{.Year}}{{ end }} {{.Holder}}
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    http://opentext.com
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.`
+`
 
 const tmplBSD = `Copyright (c){{ if .Year }} {{.Year}}{{ end }} {{.Holder}} All rights reserved.
 Use of this source code is governed by a BSD-style
